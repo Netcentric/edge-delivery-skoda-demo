19fd54701bf82d00a29e8914d51c44a1c0dcb302
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-11-08 11:35:17 +0100

    Styles improvement

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -6,12 +6,16 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
+      const button = col.querySelector('a');
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
+      }
+      if (button) {
+        button.classList.remove('button');
       }
     });
   });
