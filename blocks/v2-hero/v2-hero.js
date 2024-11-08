0527278539cc2b7d7c5b356abdb5bfe5bd100ae5
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-11-08 13:00:18 +0100

    hero improvement

--- blocks/v2-hero/v2-hero.js
+++ blocks/v2-hero/v2-hero.js
@@ -3,12 +3,16 @@ const blockName = 'v2-hero';
 export default function decorate(block) {
   const pictures = block.querySelectorAll('picture');
   const imgParent = pictures[0].parentElement;
+  const heroContainer = imgParent.parentElement;
   const newPicture = document.createElement('picture');
   const mediaQueries = ['(max-width: 749px)', '(min-width: 750px)'];
   const MQ = window.matchMedia('(min-width: 750px)');
   const img = document.createElement('img');
+  const title = block.querySelector('h1');
 
-  imgParent.classList.add(`${blockName}__container`);
+  heroContainer.classList.add(`${blockName}__container`);
+  imgParent.classList.add(`${blockName}__img-container`);
+  title.classList.add(`${blockName}__title`);
 
   mediaQueries.forEach((mediaQuery, index) => {
     const source = document.createElement('source');
